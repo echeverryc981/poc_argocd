895d50435bc3d40e8d066776a8048d176a5df719
echeverryc981 <72678807+echeverryc981@users.noreply.github.com>
2025-09-19 23:29:01 -0500

    Prueba inicial argocd 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hola Mundo desde Kubernetes 🚀"}
+    return {"message": "Hola Mundo desde Kubernetes V1 🚀"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
